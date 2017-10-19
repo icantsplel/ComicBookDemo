9a0ff7a5bf8efebec868ecf31c14abd1b3b452a0
icantsplel <31449199+icantsplel@users.noreply.github.com>
2017-10-19 14:40:24 -0400

    Add files via upload

--- ComicDemoApp/Startup.cs
+++ ComicDemoApp/Startup.cs
@@ -44,7 +44,7 @@ namespace ComicDemoApp
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            //app.UseStaticFiles();
+            app.UseStaticFiles();
             //app.UseMvcWithDefaultRoute();
             app.UseMvc(routes =>
             {
